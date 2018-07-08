4450155c3376f6aeb7c8f4b2d135699dd9d2040e
Hayk <hayk94@gmail.com>
2018-07-09 03:59:48 +0400

    SignUp view

--- src/components/Dashboard/Auth/SignUp.js
+++ src/components/Dashboard/Auth/SignUp.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-class SignUp extends Component {
+import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+
+import { Link } from 'react-router'
+
+const styles = {
+  container: {},
+  textField: {},
+  button: {
+    margin: '15px 0px'
+  }
+}
+
+class SignIn extends Component {
+  static propTypes = {
+    classes: PropTypes.object
+  }
+
   render () {
+    const {
+      classes
+    } = this.props
+
     return (
-      <div>SignUp</div>
+      <form>
+        <Grid
+          container
+          spacing={16}
+          className={classes.container}
+          alignItems='center'
+          direction='column'
+          justify='center'
+        >
+          <Typography variant='headline' gutterBottom>
+            SIGN UP
+          </Typography>
+          <TextField
+            id='email'
+            label='Email'
+            className={classes.textField}
+            value={'gdfg'}
+            onChange={() => console.log('ilog')}
+            margin='normal'
+          />
+          <TextField
+            id='password'
+            label='Password'
+            type='password'
+            className={classes.textField}
+            value={'gdfg'}
+            onChange={() => console.log('ilog')}
+            margin='normal'
+          />
+          <Button variant='contained' color='primary' className={classes.button}>
+            Sign Up
+          </Button>
+          <Link to='/sign/in'>Sign In</Link>
+        </Grid>
+      </form>
     )
   }
 }
 
-export default SignUp
+export default withStyles(styles)(SignIn)
