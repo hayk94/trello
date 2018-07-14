6882e5d29af2eac410a9dda85550880161236bea
Hayk <hayk94@gmail.com>
2018-07-14 22:09:49 +0400

    DONE fetchProjects realtime

--- src/redux/actions/projects/index.js
+++ src/redux/actions/projects/index.js
@@ -6,12 +6,20 @@ import {
 
 export const fetchProjects = () => (dispatch, getState) => {
   const projects = {}
-  db.collection('projects').get().then(querySnapshot => {
-    querySnapshot.forEach(doc => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, ' => ', doc.data())
-      projects[doc.id] = doc.data()
+  // db.collection('projects').get().then(querySnapshot => {
+  //   querySnapshot.forEach(doc => {
+  //     // doc.data() is never undefined for query doc snapshots
+  //     console.log(doc.id, ' => ', doc.data())
+  //     projects[doc.id] = doc.data()
+  //   })
+  //   dispatch({ type: ON_PROJECTS_FETCHED, payload: projects })
+  // })
+  db.collection('projects')
+    .onSnapshot(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        console.log(doc.id, ' => ', doc.data())
+        projects[doc.id] = doc.data()
+      })
+      dispatch({ type: ON_PROJECTS_FETCHED, payload: projects })
     })
-    dispatch({ type: ON_PROJECTS_FETCHED, payload: projects })
-  })
 }
--- src/redux/reducers/projects.js
+++ src/redux/reducers/projects.js
@@ -3,7 +3,7 @@ import { ON_PROJECTS_FETCHED } from '../actions/projects/types'
 export default (state = null, action) => {
   switch (action.type) {
     case ON_PROJECTS_FETCHED:
-      return action.payload
+      return { ...action.payload }
     default:
       return state
   }
