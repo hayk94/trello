fae792dd1b38d938d5b5d35f6684b882de9091ca
Hayk <hayk94@gmail.com>
2018-07-18 15:25:32 +0400

    use flow

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
+// @flow
+import * as React from 'react'
 import Grid from '@material-ui/core/Grid'
-import PropTypes from 'prop-types'
 
 import Header from './Header'
 
-class Dashboard extends Component {
-  static propTypes = {
-    children: PropTypes.node
-  }
+type Props = {
+  children: React.Node
+}
 
+class Dashboard extends React.Component<Props> {
   render () {
     return (
       <Grid container>
